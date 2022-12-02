abbe51778069893785549bfd43bd73761c59f21d
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2022-12-02 12:38:53 -0500

    Added GET functionality with Express

--- expressServer.js
+++ expressServer.js
@@ -1,8 +1,35 @@
-const { application } = require("express");
-const express = require("express");
-
+import express from "express";
+import { readFile, writeFile } from "fs/promises";
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 5000;
+// Parse the HTTP request body
+app.use(express.json());
 
-app.listen(PORT);
+// GET all pets
+app.get("/pets", (req, res) => {
+  readFile("./pets.json", "utf-8").then((data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
+// GET a single pet
+app.get("/pets/:id", (req, res) => {
+  const petIndex = req.params.id;
+  readFile("./pets.json", "utf-8").then((data) => {
+    const petsArray = JSON.parse(data);
+    if (petIndex < 0 || petIndex > petsArray.length - 1) {
+      res.status(404).set("Content-Type", "text/plain").send("Not Found");
+    } else {
+      const pet = petsArray[petIndex];
+      res.json(pet);
+    }
+  });
+});
+
+// Catch all for errors
+// app.use((err, req, res, next) => {
+//   res.status(404).send("Not Found");
+// });
+
+app.listen(PORT, () => console.log(`Server started on ports ${PORT}`));
